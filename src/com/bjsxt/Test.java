e77037120ea9052354bb67140d94cc29a15b11c2
zengpenglin949391635 <18937611392@163.com>
2019-08-12 18:29:41 +0800

    这是第一次提交

---
+++ src/com/bjsxt/Test.java
@@ -0,0 +1,15 @@
+package com.bjsxt;
+
+/**
+ * @Auther:18937
+ * @Date:2019/8/12
+ * @Description:com.bjsxt
+ * @Version:1.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
